01ac479c992eed2bb48d37f385041573eee13a92
alexanderdombroski <dom21013@byui.edu>
2024-07-08 09:03:30 -0600

    setup firebase

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Firebase
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics";
+
+import dotenv from "dotenv";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -12,6 +18,19 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const firebaseApp = initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+});
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
