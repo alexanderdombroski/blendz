4ddbf76f1e76ecc4a72f9b8295e7a575199077a8
alexanderdombroski <dom21013@byui.edu>
2024-07-18 13:40:38 -0600

    added toggable borders and ingredient images

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,15 @@ import getSpecial from './scripts/special';
 
 function App() {
   return (
-    <div className="App">
-        <main>
-          <div className='hero'>
+    <main className="App">
+        <div className='hero'>
           <img src={`${process.env.PUBLIC_URL}/images/acai-ingredients.jpg`} alt="acai ingredients hero" />
           <h1>Blendz</h1>
         </div>
-        <PageSummary pageName="Menu" href="#" hopPhrases={["Order and Pickup", `Today's Special: ${getSpecial()}`,"Fully Customizable", "Kept Cool and Fresh"]}></PageSummary>
-        <PageSummary pageName="About Us" href="#" hopPhrases={["Learn our Story", "See our Values", "Why Smoothies?"]}></PageSummary>
-        <PageSummary pageName="Contact Us" href="#" hopPhrases={["Leave a Review", "Help us improve", "Give a Suggestion"]}></PageSummary>
-        </main>
-      
-    </div>
+      <PageSummary pageName="Menu" href="#" hopPhrases={["Order and Pickup", `Today's Special: ${getSpecial()}`,"Fully Customizable", "Kept Cool and Fresh"]}></PageSummary>
+      <PageSummary pageName="About Us" href="#" hopPhrases={["Learn our Story", "See our Values", "Why Smoothies?"]}></PageSummary>
+      <PageSummary pageName="Contact Us" href="#" hopPhrases={["Leave a Review", "Help us improve", "Give a Suggestion"]}></PageSummary>
+    </main>
   );
 }
 
--- src/MenuPage.tsx
+++ src/MenuPage.tsx
@@ -1,11 +1,72 @@
-
+import { useState } from "react";
+import ingredients from "./data/ingredients.json"
+import { eventNames } from "process";
 
 function MenuPage() {
+    const [activeSection, setActiveSection] = useState<string | null>(null);
+
+    const toggleSection = (sectionName: string | null) => {
+        setActiveSection(prevSection => (prevSection === sectionName ? prevSection : sectionName));
+    };
+    
     return (
-        <div>
-            <h1>Hello</h1>
+        <main className="Menu">
+            <h1>Menu</h1>
+            
+            <nav className="menu-bar">
+                <ul>
+                    <li><button onClick={() => toggleSection('ingredients')}>Build Your Own</button></li>
+                    <li><button onClick={() => toggleSection('specialty')}>Specialty Smoothies</button></li>
+                </ul>
+            </nav>
+
+        {activeSection === 'ingredients' && (
+            <div className="section">
+                <h2>Build Your Own Smoothie</h2>
+                
+            </div>
+        )}
+
+        {(activeSection === 'specialty' || activeSection === null) && (
+            <div className="section">
+                <h2>Specialty Smoothies</h2>
+                <div className="ingredients">
+                    {ingredients["ingredient"].map((i => ingredientCard(i)))}
+                </div>
+            </div>
+        )}
+
+        </main>
+    );
+}
+
+
+interface ingredient {
+    id: number,
+    name: string,
+    image: string
+    costPerUnit: number,
+    calories: number
+}
+
+function ingredientCard(ingredient: ingredient) {
+    return (
+        <div className="ingredient" key={ingredient.id} onClick={(event) => addBorder(event)}>
+            <h3>{ingredient["name"]}</h3>
+            <img src={`${process.env.PUBLIC_URL}${ingredient["image"]}`} alt={ingredient["name"]} />
+            <h3>{ingredient["calories"]}</h3>
+            <h3>{ingredient["costPerUnit"]}</h3>
         </div>
     );
 }
+
+function addBorder(event: React.MouseEvent<HTMLDivElement>) {
+    const target = event.currentTarget as HTMLDivElement;
+    const currentBorder = target.style.borderColor;
+    if (target != null) {
+        target.style.borderColor = (currentBorder === "black") ? "#EEF8FF" : "black"
+    }
+}
+
 
 export default MenuPage;
