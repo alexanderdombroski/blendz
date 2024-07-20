8c8294506cba498b7179b075445456f3d8a99e2a
alexanderdombroski <dom21013@byui.edu>
2024-07-20 14:55:52 -0600

    added missing menu links

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
           <img src={`${process.env.PUBLIC_URL}/images/acai-ingredients.jpg`} alt="acai ingredients hero" />
           <h1>Blendz</h1>
         </div>
-      <PageSummary pageName="Menu" href="#" hopPhrases={["Order and Pickup", `Today's Special: ${getSpecial()}`,"Fully Customizable", "Kept Cool and Fresh"]}></PageSummary>
+      <PageSummary pageName="Menu" href={`${process.env.PUBLIC_URL}/menu`} hopPhrases={["Order and Pickup", `Today's Special: ${getSpecial()}`,"Fully Customizable", "Kept Cool and Fresh"]}></PageSummary>
       <PageSummary pageName="About Us" href="#" hopPhrases={["Learn our Story", "See our Values", "Why Smoothies?"]}></PageSummary>
       <PageSummary pageName="Contact Us" href="#" hopPhrases={["Leave a Review", "Help us improve", "Give a Suggestion"]}></PageSummary>
     </main>
