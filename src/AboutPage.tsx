c308b89e384014ee86305a87632533e016cfba96
alexanderdombroski <dom21013@byui.edu>
2024-07-20 16:12:13 -0600

    added hero

--- src/AboutPage.tsx
+++ src/AboutPage.tsx
@@ -2,7 +2,12 @@
 function AboutPage() {
     return (
         <main className="About">
+            <div className='hero'>
+              <img src={`${process.env.PUBLIC_URL}/images/smoothie/smoothie-row.jpg`} alt="acai ingredients hero" />
+              <h1>About Us</h1>
+            </div>
             
+
         </main>
     )
 }
