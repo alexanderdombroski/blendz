dd93aa5429b3b4a1e547f8514ad672a5ac456e04
alexanderdombroski <dom21013@byui.edu>
2024-07-20 18:38:15 -0600

    converted image to webp

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <main className="App">
         <div className='hero'>
-          <img src={`${process.env.PUBLIC_URL}/images/acai-ingredients.jpg`} alt="acai ingredients hero" />
+          <img src={`${process.env.PUBLIC_URL}/images/acai-ingredients.webp`} alt="acai ingredients hero" />
           <h1>Blendz</h1>
         </div>
       <PageSummary pageName="Menu" href={`${process.env.PUBLIC_URL}/menu`} hopPhrases={["Order and Pickup", `Today's Special: ${getSpecial()}`,"Fully Customizable", "Kept Cool and Fresh"]}></PageSummary>
